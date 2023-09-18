29386a0595ef53de5227bfc83a1d7374d2685b08
Emerson Oliveira <emerson.520@outlook.com>
2023-09-18 09:49:47 -0300

    Aprendendo a usar Metodo

---
+++ Seção_1_Classes_Atributos_Metodos_MembrosEstáticos/4-Aprendendo-Metodo/TrianguloMetodo/TrianguloMetodo/Triangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TrianguloMetodo
+{
+    internal class Triangulo
+    {
+        //Atributos da classe
+        public double A;
+        public double B;
+        public double C;
+
+        //Metodo calculo da area
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+
+    }
+}
