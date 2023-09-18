80d4720ecd36a200799b1f2f8f27660b5ed10870
Emerson Oliveira <emerson.520@outlook.com>
2023-09-18 16:51:53 -0300

    Add: Exercicio de Fixação

---
+++ Seção_1_Classes_Atributos_Metodos_MembrosEstáticos/5-ExercicioFixação/ExercicioFixacao/ExercicioFixacao/Produto.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Globalization;
+namespace ExercicioFixacao
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int qtde)
+        {
+            Quantidade += qtde;
+        }
+
+        public void RemoverProduto(int qtde)
+        {
+            Quantidade -= qtde;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2",CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$" 
+            + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Seção_1_Classes_Atributos_Metodos_MembrosEstáticos/5-ExercicioFixação/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto mercadoria = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            mercadoria.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            mercadoria.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade atual no estoque: ");
+            mercadoria.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + mercadoria);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            mercadoria.AdicionarProduto(quantidade);
+            Console.WriteLine("Dados Atualizados: " + mercadoria);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + mercadoria);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser Removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            mercadoria.RemoverProduto(quantidade);
+            Console.WriteLine("Dados Atualizados: " + mercadoria);
+        }
+    }
+}
