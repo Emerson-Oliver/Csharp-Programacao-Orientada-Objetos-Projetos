1e2839709fea04825a662106cbb15897754936ae
Emerson Oliveira <emerson.520@outlook.com>
2024-01-05 11:14:03 -0300

    Encapsulamento

---
+++ Seção_2_Construtores_PalavrasThis_Sobrecarga_Encapsulamento/2.Encapsulamento/EncapsulamentoEx01/EncapsulamentoEx01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace EmcapsulamentoEx01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Nobebook", 500.00, 10);
+
+            p.setNome("T");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+        }
+    }
+}
