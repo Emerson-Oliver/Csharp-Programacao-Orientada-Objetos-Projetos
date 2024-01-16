8f4d91bd1ea2b75b90268d7897eb2001c5b7ffaa
Emerson Oliveira <emerson.520@outlook.com>
2024-01-15 23:57:21 -0300

    Aprendendo Auto Properties

---
+++ Seção_2_Construtores_PalavrasThis_Sobrecarga_Encapsulamento/4.AutoProperties/AutoProperties/AutoProperties/Produtos.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+using System.Runtime;
+
+namespace AutoProperties
+{
+    class Produto
+    {
+        private string _nome; // Não faz AutoProprties nesse caso pois tem uma logica particular
+        public double Preco { get; private set; } //private indica que o preço não pode ser alterado por outros arquivos
+        public int Quantidade { get; private set; }
+
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        /* public double Preco
+         {
+             get { return _preco; }
+         } */
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Seção_2_Construtores_PalavrasThis_Sobrecarga_Encapsulamento/4.AutoProperties/AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using System.Globalization;
+
+namespace AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Nobebook", 500.00, 10);
+
+            p.Nome = "N";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
+
+
